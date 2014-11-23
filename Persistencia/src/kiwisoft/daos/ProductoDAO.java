4e2699c5b892e6ad7df67925907c505e74733f4b
denial Mac <geminisdany@gmail.com>
2014-11-23 04:22:48 +0100

    buscador en produtoDAO

--- Persistencia/src/kiwisoft/daos/ProductoDAO.java
+++ Persistencia/src/kiwisoft/daos/ProductoDAO.java
@@ -63,9 +63,14 @@ public class ProductoDAO {
 	}
 	
 	
-	//busqueda simple por nombre
-	public Producto buscarProductoNombre(String nombre){
+	//busqueda simple por nombre EXACTO
+	public Producto buscarProductoNombreExacto(String nombre){
 		return em.createQuery("select u from Producto u where u.nombre='"+nombre+"'",Producto.class).getSingleResult();
+	}
+	
+	//busqueda simple por nombre, contiene el nombre
+	public Collection<Producto> buscarProductoNombre(String nombre){
+		return em.createQuery("select u from Producto u where lower(u.nombre) like lower('%"+nombre+"%')",Producto.class).getResultList();
 	}
 	
 	//busqueda simple por tipo
@@ -73,22 +78,37 @@ public class ProductoDAO {
 		return em.createQuery("select u from Producto u where u.tipo='"+tipo+"'",Producto.class).getResultList();
 	}
 	
-	//busqueda simple por proveedor
-	public Collection<Producto> buscarProductoProveedor(String proveedor){
+	//busqueda simple por proveedor EXACTO
+	public Collection<Producto> buscarProductoProveedorExacto(String proveedor){
 		return em.createQuery("select u from Producto u where u.proveedor.nombre='"+proveedor+"'",Producto.class).getResultList();
 	}
 	
+	//busqueda simple por proveedor
+		public Collection<Producto> buscarProductoProveedor(String proveedor){
+			return em.createQuery("select u from Producto u where lower(u.proveedor.nombre) like lower('%"+proveedor+"%')",Producto.class).getResultList();
+		}
+	
 	///busqueda Basica por Precio
-	public Collection<Producto> buscarProductoMayor(Float precio){
-		return em.createQuery("select u from Producto u where u.precio>='"+precio+"'",Producto.class).getResultList();
+	public Collection<Producto> buscarProductoMayor(double precio){
+		return em.createQuery("select u from Producto u where u.precio>'"+precio+"'",Producto.class).getResultList();
 	}
 	
-	public Collection<Producto> buscarProductoMenor(Float precio){
-		return em.createQuery("select u from Producto u where u.precio<='"+precio+"'",Producto.class).getResultList();
+	public Collection<Producto> buscarProductoMenor(double precio){
+		return em.createQuery("select u from Producto u where u.precio<'"+precio+"'",Producto.class).getResultList();
 	}
 	
-	public Collection<Producto> buscarProductoIgual(Float precio){
+	public Collection<Producto> buscarProductoIgual(double precio){
 		return em.createQuery("select u from Producto u where u.precio='"+precio+"'",Producto.class).getResultList();
+	}
+
+	public Collection<Producto> buscarAvanzado(String campo1,String valor1, String operador, String campo2,String valor2) {
+		// TODO Auto-generated method stub
+		if(campo1.equals("proveedor"))
+			campo1="proveedor.nombre";
+		if(campo2.equals("proveedor"))
+			campo2="proveedor.nombre";
+		
+		return em.createQuery("select u from Producto u where lower(u."+campo1+") like lower('%"+valor1+"%') "+operador+" u."+campo2+" like lower('%"+valor2+"%')",Producto.class).getResultList();
 	}
 	
 	
